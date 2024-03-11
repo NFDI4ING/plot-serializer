5c8ae4d8bb27b38e168ce8aba2ada4cdb8ebe73f
thomas.wu <thomas.wu@stud.tu-darmstadt.de>
2024-03-11 21:20:40 +0100

    Test for plot Surface3D

---
+++ tests/test_surface_plot3D.py
@@ -0,0 +1,53 @@
+import json
+from typing import Any
+import numpy as np
+from plot_serializer.matplotlib.serializer import MatplotlibSerializer
+from tests import read_plot
+from mpl_toolkits.mplot3d.axes3d import Axes3D as MplAxes3D
+
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+
+
+def test_simple() -> None:
+
+    serializer = MatplotlibSerializer()
+    fig, ax = serializer.subplots(subplot_kw={"projection": "3d"})
+
+    x = np.arange(-2, 2, 0.5)
+    y = np.arange(-2, 2, 0.5)
+    x, y = np.meshgrid(x, y)
+    z = -(x**2 + y**2)
+
+    ax.plot_surface(x, y, z)
+
+    json_string = serializer.to_json(emit_warnings=False)
+    output = json.loads(json_string)
+    expected = json.loads(read_plot("surface_plot3D_simple"))
+
+    assert output == expected
+
+
+def test_all_features() -> None:
+    serializer = MatplotlibSerializer()
+    fig, ax = serializer.subplots(subplot_kw={"projection": "3d"})
+
+    x = np.outer(np.linspace(-3, 3, 20), np.ones(20))
+    y = x.copy().T  # transpose
+    z = np.sin(x**2) + np.cos(y**2)
+
+    ax.plot_surface(x, y, z, color="yellow", label="testSurface")
+
+    ax.set_xlabel("labelX")
+    ax.set_ylabel("labelY")
+    ax.set_zlabel("labelZ")
+
+    ax.set_title("testTitle")
+
+    json_string = serializer.to_json(emit_warnings=False)
+    output = json.loads(json_string)
+    expected = json.loads(read_plot("surface_plot3D_all_features"))
+
+    assert output == expected
